664ecccb333381cdb34616b398fd9e4477d699d3
ericwang0717 <eric.wang0717@gmail.com>
2014-05-07 23:58:44 +0800

    finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  
+  
+  def User.new_remember_token
+      SecureRandom.urlsafe_base64
+    end
+
+    def User.hash(token)
+      Digest::SHA1.hexdigest(token.to_s)
+    end
+
+    private
+
+      def create_remember_token
+        self.remember_token = User.hash(User.new_remember_token)
+      end
+  
 end
